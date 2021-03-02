90673e744cf7beec381c8dd0715facea077dd65d
rlawnsdnjs <79901573+rlawnsdnjs@users.noreply.github.com>
2021-03-02 17:35:21 +0900

    Add files via upload

--- Work/Week1/Test1.cpp
+++ Work/Week1/Test1.cpp
@@ -20,15 +20,14 @@ main()
 
 	printf("COVID%d, Get out!\n",19);
 
-	printf("%8d\n",123);	
+	printf("%8d\n",123);
 	printf("%-8d\n",4);
 
 	printf("%d %8d %8d \n", 123,456,789);
 	printf("%-8d %d %8d \n",123,456,789);
 	printf("%-8d %-8d %d \n",123,456,789);
 	printf("real number의 출력 : %8.2f %10.3f \n",3.14159265,456.789123);
-	printf("",);
-	
+
 	printf("%d \n", 1234);
 	printf("%d %d %d \n",10,20,534234);
 	printf("ookini\n");
@@ -42,6 +41,7 @@ main()
     printf("data type별 byte수(float) : %d byte \n",sizeof(float));
     printf("data type별 byte수(double) : %d byte \n",sizeof(double));
     
+    printf("The new Mac arrive at home, the work what I do is a lot maybe.");    
 	return 0;
 }
 
