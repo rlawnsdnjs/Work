97b96865351b605ae911d1b7c0894e3f4b40e598
rlawnsdnjs <79901573+rlawnsdnjs@users.noreply.github.com>
2021-03-04 22:28:57 +0900

    Update Size.cpp

--- Practice/Size.cpp
+++ Practice/Size.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#define MONTHS 12  // sangsoo 12
+#define MONTHS 12  // 상수값 지정.
 
 int main(void)
 {
-	double monthsalary = 1000.5;
+	double monthsalary = 1000.5;  // 한달 월급 계산
 	printf("$ %.2f",monthsalary * MONTHS);
 	return 0;
 }
