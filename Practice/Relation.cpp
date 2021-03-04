0276b9ba39ecd375e033f016731653ebaa653ae3
rlawnsdnjs <79901573+rlawnsdnjs@users.noreply.github.com>
2021-03-04 22:46:34 +0900

    Add files via upload

---
+++ Practice/Relation.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 30, y = 50;
+	printf("Are x and y same? %d\n", x==y);
+	printf("Are x and y different? %d\n", x!=y);
+	printf("Is x bigger than y? %d\n", x>y);
+	printf("Is x smaller than y? %d\n", x>y);
+	printf("How x value input to y? %d", x=y);
+	return 0;
+}
