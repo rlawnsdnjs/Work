de483677e9e70eeb3d8d4432877d37396bc19b44
rlawnsdnjs <79901573+rlawnsdnjs@users.noreply.github.com>
2021-03-04 22:28:06 +0900

    Update Plus Minus.cpp

--- Practice/Plus Minus.cpp
+++ Practice/Plus Minus.cpp
@@ -4,11 +4,11 @@ int main()
 {
 	int x = 0;
 	printf("This is %d now.\n", x);
-	x++;  // E sun ggaji +1 han gut.
+	x++;  // 이 줄까지 연산만 한것. (다음줄부터 증가된 채로 출력됨.)
 	printf("This is %d now.\n", x);
-	printf("This is %d now.\n", x--);  // E sun ggaji gyesan nono.
+	printf("This is %d now.\n", x--);  // 이 줄까지 계산 ㄴㄴ
 	printf("This is %d now.\n", x);
-	printf("This is %d now.\n", --x);  // E sun ggaji gyesan yes.
+	printf("This is %d now.\n", --x);  // 이 줄까지 계산 ㅇㅇ
 	
 	return 0;
 }
