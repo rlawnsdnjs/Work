c659c644aac79f27607477887a911a76932e6257
rlawnsdnjs <79901573+rlawnsdnjs@users.noreply.github.com>
2021-03-09 22:03:05 +0900

    Update math and english and total.c

--- Practice/math and english and total.c
+++ Practice/math and english and total.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-	int score[5][2];
-	int total[2] = { 0, };
+	int score[5][2];  // 5명의 학생에 2개의 과목
+	int total[2] = { 0, };  // { 0, } : 1차원 배열을 할당할 때, 모든 값에 0을 대입하겠다는 뜻.
 	int i;
 	
 	for(i=0; i<5; i++)
